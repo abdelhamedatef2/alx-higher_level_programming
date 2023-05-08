9a1d6578a9caa479cfd535e78316c38a0c483488
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-05-08 14:25:32 -0700

    solve mandotary tasks

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -7,6 +7,8 @@
  * struct listint_s - singly linked list
  * @n: Int
  * @next: pointer to next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct listint_s
 {
@@ -15,11 +17,8 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-
 listint_t *add_nodeint(listint_t **head, const int n);
-
 void free_listint(listint_t *head);
-
 int check_cycle(listint_t *list);
 
 #endif
