b20a554d3e90f8e301eda6fde843fd953832d370
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-05-30 07:10:27 -0700

    solve 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""definition square and its size"""
+
+
+class Square:
+    """implementation square"""
+
+    def __init__(self, size=0):
+        """initialization ofnew square with size arg"""
+        self.__size = size
