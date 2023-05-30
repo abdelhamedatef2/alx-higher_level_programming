ad4bba8a2c37674791ada792aa91c8a5fa0720d9
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-05-30 07:08:17 -0700

    hot fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""definition of a square"""
+
 
 class Square:
-    # square is a shape that has equal dimensions
+    """square is a shape that has equal dimensions"""
     pass
