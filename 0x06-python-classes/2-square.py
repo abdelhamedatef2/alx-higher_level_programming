6bba38a364d1193f373b5d8521055eddd0d3e6fe
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-05-30 07:40:43 -0700

    fix code

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,10 @@
 class Square:
     """initialization of size as an arg"""
     def __init__(self, size=0):
-        """initialization of new square with size arg"""
-        self.__size = size
+        """initialization of new square with size"""
+
         if not isinstance(size, int):
             raise TypeError('size should be int')
         if size < 0:
             raise ValueError('size should be >= 0')
+                self.__size = size
