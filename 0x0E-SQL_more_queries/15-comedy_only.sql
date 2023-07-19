ce9d97eb9199a824b0360780c2695159d4398f27
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-19 18:01:20 +0300

    Create 15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- list comedy only
+-- list comedy only
+SELECT tv_shows.title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
