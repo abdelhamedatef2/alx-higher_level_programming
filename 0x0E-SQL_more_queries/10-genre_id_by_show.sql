2e718c9e0f665f802df7dd169dcb69c6c8e5ad45
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-19 17:59:51 +0300

    Create 10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- script list show contained show
+-- list all show
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
