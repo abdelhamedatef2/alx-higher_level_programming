07dc0bda798f766c45cdb10dd403fa5daf2a7e5f
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-19 17:56:58 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- This lists all privileges of the users user_0d_1 and user_0d_2
+-- script show privilages
+-- statement show all privilage of useres
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- show other user
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
