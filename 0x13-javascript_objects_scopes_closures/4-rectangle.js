d91545998fd668bcfb9027cd63a7cabe38d92c62
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-08-15 08:15:35 -0700

    creat two new methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print ()
+     {
+      for (let i = 0; i < this.height; i++)
+       {
+        let x = '';
+        for (let j = 0; j < this.width; j++) 
+        {
+          x += 'X';
+        }
+        console.log(x);
+      }
+    }
+  rotate(){
+    let temp;
+    temp=this.width;
+    this.width=this.height;
+    this.height=temp;
+}
+double(){
+    this.width*=2;
+    this.height*=2;
+}
+}
+  module.exports = Rectangle;
