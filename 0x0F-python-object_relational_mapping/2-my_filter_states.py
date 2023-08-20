eca39ab8ea10a03909d5f6d200706ce5b62eabd0
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-08-19 18:28:29 -0700

    change query and use binary function

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" module to filter DB acording to atgv provided """
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """ finc not run when imported as module """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+    start = db.cursor()
+    start.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}'\
+                  ORDER BY states.id".format(argv[4]))
+    rows = start.fetchall()
+    for row in rows:
+        print(row)
+    start.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()#!/usr/bin/python3
+""" module to fillter DB according to atgv provided """
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """ func not run when imported as module """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+    start = db.cursor()
+    start.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}'\
+                  ORDER BY states.id".format(argv[4]))
+    rows = start.fetchall()
+    for row in rows:
+        print(row)
+    start.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
