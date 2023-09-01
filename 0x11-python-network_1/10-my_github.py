383c45a4fedb091026a6fbbb2b8247ec308e047d
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-09-01 18:09:10 +0300

    Create 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script takes GitHub credentials
+and uses GitHub API to display user id
+"""
+from requests import get, auth
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
